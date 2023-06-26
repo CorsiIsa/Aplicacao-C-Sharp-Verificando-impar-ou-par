97b74ca8db242b146540d9aa2289c33ce872afef
CorsiIsa <icorsi15@gmail.com>
2023-06-26 17:29:44 -0300

    Verficando se o numero é impar ou par

---
+++ Win_Exemplo01/Frm.Exemplo01.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Win_Exemplo01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnVerifica = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "digite um numero";
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(139, 34);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSair);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Controls.Add(this.btnVerifica);
+            this.panel1.Location = new System.Drawing.Point(30, 206);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(378, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnVerifica
+            // 
+            this.btnVerifica.Location = new System.Drawing.Point(19, 17);
+            this.btnVerifica.Name = "btnVerifica";
+            this.btnVerifica.Size = new System.Drawing.Size(75, 23);
+            this.btnVerifica.TabIndex = 0;
+            this.btnVerifica.Text = "&Verifica";
+            this.btnVerifica.UseVisualStyleBackColor = true;
+            this.btnVerifica.Click += new System.EventHandler(this.btnVerifica_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(134, 17);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(250, 17);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(432, 328);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Verifica Números";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnumero;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnVerifica;
+    }
+}
+
---
+++ Win_Exemplo01/Frm.Exemplo01.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Exemplo01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerifica_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int x = int.Parse(txtnumero.Text);
+                if (x % 2 == 0)
+                {
+                    MessageBox.Show("O número  " + x + " é PAR");
+                }
+                else
+                {
+                    MessageBox.Show("O número  " + x + " é IMPAR");
+                }
+            }
+
+            catch(FormatException erro)
+            {
+                MessageBox.Show(erro.Message);
+                btnCancelar_Click(sender, e);
+            }
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtnumero.Clear();// Limpa o conteúdo digitado
+            txtnumero.Focus();// Faz o cursor ficar intermitente
+        }
+    }
+}
